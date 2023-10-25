6417b6851bb35bab91cd749623559a9364e9a30f
WavyBoyJodii <najeecasey@gmail.com>
2023-10-25 00:41:53 -0400

    reset 404 catcher

--- src/app.ts
+++ src/app.ts
@@ -131,9 +131,9 @@ app.use('/', indexRouter);
 app.use('/user', passport.authenticate('jwt', { session: false }), usersRouter);
 
 // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function (
